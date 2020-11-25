3eb8453be1695c896c79b2b17621cc141acea318
David Madouros <david@earthvectors.com>
2020-11-24 23:02:37 -0700

    Implement to_customer_info

---
+++ lib/widgets_inc/util/constrained_type.rb
@@ -0,0 +1,54 @@
+module WidgetsInc
+  module Util
+    module ConstrainedType
+      extend ::Dry::Monads[:result, :maybe]
+
+      class << self
+        def create_string
+          -> (field_name, max_length, value) do
+            schema = Dry::Schema.Params do
+              required(:value).filled(:string, max_size?: max_length)
+            end
+
+            validate_schema.(schema, field_name, value)
+          end
+        end
+
+        def create_string_option
+          -> (field_name, max_length, value) do
+            schema = Dry::Schema.Params do
+              required(:value).filled(:string, max_size?: max_length)
+            end
+
+            validate_schema.(schema, field_name, value)
+              .fmap { |r| Some(r) }
+          end
+        end
+
+
+        def create_like
+          -> (field_name, pattern, value) do
+            schema = Dry::Schema.Params do
+              required(:value).filled(:string, format?: pattern)
+            end
+
+            validate_schema.(schema, field_name, value)
+          end
+        end
+
+        private
+
+        def validate_schema
+          -> (schema, field_name, value) do
+            schema.call(value: value)
+              .to_monad
+              .fmap { |r| r.to_h[:value] }
+              .or { |r|
+                Failure(r.errors.to_h[:value].map { |e| "#{field_name}: #{e}" })
+              }
+          end
+        end
+      end
+    end
+  end
+end
